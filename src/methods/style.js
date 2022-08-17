db6c77d370e865e1ffa0380cd2367e8a708c780b
blackwolf449 <tacvini@gmail.com>
2022-08-16 21:35:25 -0300

    style and login form

--- src/methods/style.js
+++ src/methods/style.js
@@ -1,6 +1,14 @@
 export const style = {
-    navbar: 'navbar',
+    navbar: 'navbar is-transparent',
     navbarBurger: 'navbar-burger',
     navbarBrand: 'navbar-brand',
     navbarItem: 'navbar-item',
+    navbarEnd: 'navbar-end',
+    fieldIsGrouped: 'field is-grouped',
+    btnInfoLight: 'button is-info is-light',
+    btnInfo: 'button is-info',
+    btnWarningLight: 'button is-warning is-light',
+    btnPrimaryLight: 'button is-primary is-light',
+    loginForm: 'formLogin card',
+    inputDefault: 'input is-hovered',
 }
--- src/views/Controller.vue
+++ src/views/Controller.vue
@@ -1,17 +1,35 @@
 <template>
-    <nav :class="style.navbar" role="navigation" aria-label="main navigation">
+<nav :class="style.navbar">
   <div :class="style.navbarBrand">
-    <a :class="style.navbarItem">
+    <a :class="style.navbarItem" href="#">
       <span><ion-icon name="alarm-outline" size="large"></ion-icon>TacToc</span>
     </a>
-
-    <a role="button" :class="style.navbarBurger" aria-label="menu" aria-expanded="false">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
   </div>
+
+  
+    <div :class="style.navbarEnd">
+      <div :class="style.navbarItem">
+        <div :class="style.fieldIsGrouped">
+          <button v-if="!isSingIn" :class="style.btnWarningLight">Register</button>
+          <button v-if="!isSingIn" :class="style.btnPrimaryLight" @click="form.login = !form.login">Login</button>
+        </div>
+      </div>
+    </div>
 </nav>
+
+<div :class="style.loginForm" v-if="form.login">
+  <h1>Login</h1>
+  <hr>
+  <label>
+    Username:
+    <input type="text" :class="style.inputDefault">
+  </label>
+  <label>
+    Password:
+    <input type="text" :class="style.inputDefault">
+  </label>
+  <button :class="style.btnInfo">Login</button>
+</div>
 </template>
 
 <script>
@@ -20,7 +38,9 @@ export default {
     name: "Controller",
     data(){
       return {
-        style: style
+        style: style,
+        isSingIn: false,
+        form: {login: false, register: false}
       }
     }
 }
@@ -28,4 +48,7 @@ export default {
 
 <style lang="scss" scoped>
  @import '../scss/style.scss';
+ .card button{
+  margin-bottom: 10px;
+ }
 </style>
